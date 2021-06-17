dd85f2a3ac0cc834b4170af6de95d26589ed42fc
emilymahoney <ermahoney@wpi.edu>
2021-06-16 21:47:46 -0400

    added test cases and made pinpoint object

---
+++ src/Pinpoint.java
@@ -0,0 +1,37 @@
+public class Pinpoint {
+
+    public final double x;
+    public final double y;
+
+    public Pinpoint(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public static void main (String[] args){
+        Pinpoint pinpoint1 = new Pinpoint(-3,-5);
+        Pinpoint pinpoint2 = new Pinpoint(3,5);
+
+        //testing out Euclidean distance calculator
+        double eDistPinpoints = eDistance(pinpoint1, pinpoint2);
+
+        //testing out Manhattan distance calculator
+        double mDistPinpoints = mDistance(pinpoint1, pinpoint2);
+        System.out.print("For points (" + pinpoint1.x + "," + pinpoint1.y + ") and (" + pinpoint2.x + "," + pinpoint2.y + "), Euclidean Distance = " + eDistPinpoints + " and Manhattan Distance = " + mDistPinpoints);
+    }
+
+    //Euclidean distance calculator
+    public static double eDistance(Pinpoint pinpoint1, Pinpoint pinpoint2){
+        double x = Math.pow(pinpoint2.x - pinpoint1.x, 2);
+        double y = Math.pow(pinpoint2.y - pinpoint1.y, 2);
+        double eDist = Math.sqrt(x + y);
+        return eDist;
+    }
+
+    //Manhattan distance calculator
+    public static double mDistance(Pinpoint pinpoint1, Pinpoint pinpoint2){
+        double mDist = Math.abs(pinpoint2.x - pinpoint1.x) + Math.abs(pinpoint2.y - pinpoint1.y);
+        return mDist;
+    }
+
+}
---
+++ src/TestPinpoint.java
@@ -0,0 +1,34 @@
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestPinpoint {
+
+    @Test
+    void testEuclideanDist() {
+        Pinpoint pinpoint1 = new Pinpoint(-3,-5);
+        Pinpoint pinpoint2 = new Pinpoint(3,5);
+        assertEquals(11.661903789690601, Pinpoint.eDistance(pinpoint1, pinpoint2));
+    }
+
+    @Test
+    void testEuclideanDistSameSpot() {
+        Pinpoint pinpoint1 = new Pinpoint(3,5);
+        Pinpoint pinpoint2 = new Pinpoint(3,5);
+        assertEquals(0.0, Pinpoint.eDistance(pinpoint1, pinpoint2));
+    }
+
+    @Test
+    void testManhattanDist() {
+        Pinpoint pinpoint1 = new Pinpoint(-3,-5);
+        Pinpoint pinpoint2 = new Pinpoint(3,5);
+        assertEquals(16.0, Pinpoint.mDistance(pinpoint1, pinpoint2));
+    }
+
+    @Test
+    void testManhattanDistSameSpot() {
+        Pinpoint pinpoint1 = new Pinpoint(3,5);
+        Pinpoint pinpoint2 = new Pinpoint(3,5);
+        assertEquals(0.0, Pinpoint.mDistance(pinpoint1, pinpoint2));
+    }
+}
