3d91cd503764fe7cb3213b57b98dfbfed155df43
emilymahoney <ermahoney@wpi.edu>
2021-06-23 14:47:17 -0400

    fixed factors to be of variable size, and 0.0 to skip factor

--- src/Route.java
+++ src/Route.java
@@ -1,43 +1,47 @@
+import java.util.LinkedList;
 import java.util.Vector;
 
 public class Route {
-    public double danger;
-    public double unavailability;
-    public double routeDistance;
+    public static double danger;
+    public static double unavailability;
+    public static double routeDistance;
+    public static LinkedList<Double> factors;
 
-    public Route(){
-        this.danger = danger;
-        this.unavailability = unavailability;
-        this.routeDistance = routeDistance;
+
+    public Route(LinkedList<Double> factors){
+        this.danger = getDanger();
+        this.unavailability = getUnavailability();
+        this.routeDistance = getRouteDistance();
     }
 
     public static void main (String[] args){
+        factors = new LinkedList<>();
+        Route route1 = new Route(factors);
 
-        Route route1 = new Route();
+        factors.add(0, route1.getDanger());
+        factors.add(1, route1.getUnavailability());
+        factors.add(2, route1.getRouteDistance());
 
-        Vector<Double> myWeights = new Vector<Double>();
+        Vector<Double> myWeights = new Vector<>();
         myWeights.add(0.25);
         myWeights.add(0.50);
         myWeights.add(0.75);
         //testing out weighted sum calculator
-        double routeScore = weightedSumCalculator(myWeights, 3, route1);
-        System.out.println("The route score for route1 is: " + routeScore);
+        double routeScore = weightedSumCalculator(myWeights, route1);
+        System.out.println("The route score for the given weight values is: " + routeScore);
     }
 
     //number of factors is specified at runtime
     //input: weights, numWeights
-    //output: sum
+    //output: weightedSum
     //for a given route, do factor methods times each weight and add up
-    public static double weightedSumCalculator( Vector<Double> weights, int numWeights, Route route){
-        weights.setSize(numWeights);
+    public static double weightedSumCalculator( Vector<Double> weights, Route myRoute){
         double weightedSum = 0;
         for( int i = 0; i < weights.size(); i++ ){
-            if( i == 0 ){
-                weightedSum += weights.get(i) * route.getDanger();
-            } else if( i == 1 ){
-                weightedSum += weights.get(i) * route.getUnavailability();
-            } else if( i == 2 ){
-                weightedSum += weights.get(i) * route.getRouteDistance();
+            if( weights.get(i) == 0.0){
+                continue;
+            } else {
+                weightedSum += weights.get(i) * myRoute.factors.get(i);
             }
         }
         return weightedSum;
--- src/TestRoute.java
+++ src/TestRoute.java
@@ -1,92 +1,131 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.LinkedList;
 import java.util.Vector;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TestRoute {
-    //different numbers of factors by changing number of weights
-    //different numbers of weights different values for weights
 
     @Test
-    void testWeightedSumThreeWeightsInitialWeightValues() {
-        Route myRoute = new Route();
-        Vector<Double> myWeights = new Vector<Double>();
+    void testWeightedSumThreeWeights() {
+
+        Route.factors = new LinkedList<>();
+        Route myRoute = new Route(Route.factors);
+        Route.factors.add(0, myRoute.getDanger());
+        Route.factors.add(1, myRoute.getUnavailability());
+        Route.factors.add(2, myRoute.getRouteDistance());
+        Vector<Double> myWeights = new Vector<>();
         myWeights.add(0.25);
         myWeights.add(0.50);
         myWeights.add(0.75);
-        assertEquals(1.2000000000000002, Route.weightedSumCalculator(myWeights, 3, myRoute));
+        assertEquals(1.2000000000000002, Route.weightedSumCalculator(myWeights, myRoute));
     }
 
     @Test
-    void testWeightedSumThreeWeightsDiffWeightValues() {
-        Route myRoute = new Route();
-        Vector<Double> myWeights = new Vector<Double>();
+    void testWeightedSumThreeWeightsFirstOneZero() {
+        Route.factors = new LinkedList<>();
+        Route myRoute = new Route(Route.factors);
+        Route.factors.add(0, myRoute.getDanger());
+        Route.factors.add(1, myRoute.getUnavailability());
+        Route.factors.add(2, myRoute.getRouteDistance());
+
+        Vector<Double> myWeights = new Vector<>();
+        myWeights.add(0.0);
         myWeights.add(1.0);
         myWeights.add(1.0);
-        myWeights.add(1.0);
-        assertEquals(2.4000000000000004, Route.weightedSumCalculator(myWeights, 3, myRoute));
+        assertEquals(1.6, Route.weightedSumCalculator(myWeights, myRoute));
     }
 
     @Test
-    void testWeightedSumThreeWeightsZeroWeightValues() {
-        Route myRoute = new Route();
-        Vector<Double> myWeights = new Vector<Double>();
+    void testWeightedSumThreeWeightsSecondOneZero() {
+        Route.factors = new LinkedList<>();
+        Route myRoute = new Route(Route.factors);
+        Route.factors.add(0, myRoute.getDanger());
+        Route.factors.add(1, myRoute.getUnavailability());
+        Route.factors.add(2, myRoute.getRouteDistance());
+        Vector<Double> myWeights = new Vector<>();
+        myWeights.add(0.25);
         myWeights.add(0.0);
-        myWeights.add(0.0);
-        myWeights.add(0.0);
-        assertEquals(0.0, Route.weightedSumCalculator(myWeights, 3, myRoute));
+        myWeights.add(0.75);
+        assertEquals(0.8, Route.weightedSumCalculator(myWeights, myRoute));
     }
 
     @Test
-    void testWeightedSumTwoWeightsInitialWeightValues() {
-        Route myRoute = new Route();
-        Vector<Double> myWeights = new Vector<Double>();
+    void testWeightedSumThreeWeightsThirdOneZero() {
+        Route.factors = new LinkedList<>();
+        Route myRoute = new Route(Route.factors);
+        Route.factors.add(0, myRoute.getDanger());
+        Route.factors.add(1, myRoute.getUnavailability());
+        Route.factors.add(2, myRoute.getRouteDistance());
+        Vector<Double> myWeights = new Vector<>();
         myWeights.add(0.25);
         myWeights.add(0.50);
-        assertEquals(0.6000000000000001, Route.weightedSumCalculator(myWeights, 2, myRoute));
-    }
-
-    @Test
-    void testWeightedSumTwoWeightsDiffWeightValues() {
-        Route myRoute = new Route();
-        Vector<Double> myWeights = new Vector<Double>();
-        myWeights.add(1.0);
-        myWeights.add(1.0);
-        assertEquals(1.6, Route.weightedSumCalculator(myWeights, 2, myRoute));
-    }
-
-    @Test
-    void testWeightedSumTwoWeightsZeroWeightValues() {
-        Route myRoute = new Route();
-        Vector<Double> myWeights = new Vector<Double>();
         myWeights.add(0.0);
-        myWeights.add(0.0);
-        assertEquals(0.0, Route.weightedSumCalculator(myWeights, 2, myRoute));
+        assertEquals(0.6000000000000001, Route.weightedSumCalculator(myWeights, myRoute));
     }
 
     @Test
-    void testWeightedSumOneWeightInitialWeightValues() {
-        Route myRoute = new Route();
-        Vector<Double> myWeights = new Vector<Double>();
+    void testWeightedSumTwoWeights() {
+        Route.factors = new LinkedList<>();
+        Route myRoute = new Route(Route.factors);
+        Route.factors.add(0, myRoute.getDanger());
+        Route.factors.add(1, myRoute.getUnavailability());
+        Route.factors.add(2, myRoute.getRouteDistance());
+        Vector<Double> myWeights = new Vector<>();
         myWeights.add(0.25);
-        assertEquals(0.2, Route.weightedSumCalculator(myWeights, 1, myRoute));
+        myWeights.add(0.50);
+        assertEquals(0.6000000000000001, Route.weightedSumCalculator(myWeights, myRoute));
     }
 
     @Test
-    void testWeightedSumOneWeightDiffWeightValues() {
-        Route myRoute = new Route();
-        Vector<Double> myWeights = new Vector<Double>();
-        myWeights.add(1.0);
-        assertEquals(0.8, Route.weightedSumCalculator(myWeights, 1, myRoute));
+    void testWeightedSumTwoWeightsFirstZero() {
+        Route.factors = new LinkedList<>();
+        Route myRoute = new Route(Route.factors);
+        Route.factors.add(0, myRoute.getDanger());
+        Route.factors.add(1, myRoute.getUnavailability());
+        Route.factors.add(2, myRoute.getRouteDistance());
+        Vector<Double> myWeights = new Vector<>();
+        myWeights.add(0.0);
+        myWeights.add(0.50);
+        assertEquals(0.4, Route.weightedSumCalculator(myWeights, myRoute));
+    }
+
+    @Test
+    void testWeightedSumTwoWeightsSecondZero() {
+        Route.factors = new LinkedList<>();
+        Route myRoute = new Route(Route.factors);
+        Route.factors.add(0, myRoute.getDanger());
+        Route.factors.add(1, myRoute.getUnavailability());
+        Route.factors.add(2, myRoute.getRouteDistance());
+        Vector<Double> myWeights = new Vector<>();
+        myWeights.add(0.25);
+        myWeights.add(0.0);
+        assertEquals(0.2, Route.weightedSumCalculator(myWeights, myRoute));
+    }
+
+    @Test
+    void testWeightedSumOneWeight() {
+        Route.factors = new LinkedList<>();
+        Route myRoute = new Route(Route.factors);
+        Route.factors.add(0, myRoute.getDanger());
+        Route.factors.add(1, myRoute.getUnavailability());
+        Route.factors.add(2, myRoute.getRouteDistance());
+        Vector<Double> myWeights = new Vector<>();
+        myWeights.add(0.25);
+        assertEquals(0.2, Route.weightedSumCalculator(myWeights, myRoute));
     }
 
     @Test
     void testWeightedSumOneWeightZeroWeightValues() {
-        Route myRoute = new Route();
-        Vector<Double> myWeights = new Vector<Double>();
+        Route.factors = new LinkedList<>();
+        Route myRoute = new Route(Route.factors);
+        Route.factors.add(0, myRoute.getDanger());
+        Route.factors.add(1, myRoute.getUnavailability());
+        Route.factors.add(2, myRoute.getRouteDistance());
+        Vector<Double> myWeights = new Vector<>();
         myWeights.add(0.0);
-        assertEquals(0.0, Route.weightedSumCalculator(myWeights, 1, myRoute));
+        assertEquals(0.0, Route.weightedSumCalculator(myWeights, myRoute));
     }
 
 }
