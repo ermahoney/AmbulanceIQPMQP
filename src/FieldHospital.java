7f733ab6377dec061bb898c0c7e8fca70628ac18
emilymahoney <ermahoney@wpi.edu>
2021-07-06 15:45:19 -0400

    Added coordinates and distance calculator for getroutedistance. Trouble with factors

--- src/FieldHospital.java
+++ src/FieldHospital.java
@@ -1,31 +1,33 @@
-import java.lang.reflect.Array;
-import java.lang.reflect.Field;
 import java.util.*;
 
 public class FieldHospital {
     private String fHName;
+    private Pinpoint fHLocation;
     private double danger;
     private double unavailability;
     private double routeDistance;
     public static LinkedList<Double> factors;
     public static LinkedList<String> fieldHospitals;
-    public static double poi;
 
 
-    public FieldHospital(LinkedList<Double> factors) {
+    public FieldHospital(LinkedList<Double> factors, Pinpoint fHLocation, Pinpoint poi) {
         this.fHName = fHName;
+        this.factors = factors;
+        this.fHLocation = fHLocation;
         this.danger = getDanger();
         this.unavailability = getUnavailability();
-        this.routeDistance = getRouteDistance();
+        this.routeDistance = getRouteDistance(fHLocation, poi);
     }
 
     public static void main(String[] args) {
-        factors = new LinkedList<>();
-        FieldHospital fh1 = new FieldHospital(factors);
+        LinkedList<Double> factors1 = new LinkedList<>();
+        Pinpoint poi = new Pinpoint(0,0);
+        Pinpoint locationFH1 = new Pinpoint(-3,-5);
+        FieldHospital fh1 = new FieldHospital(factors1, locationFH1, poi);
 
-        factors.add(0, fh1.getDanger());
-        factors.add(1, fh1.getUnavailability());
-        factors.add(2, fh1.getRouteDistance());
+        factors1.add(0, fh1.getDanger());
+        factors1.add(1, fh1.getUnavailability());
+        factors1.add(2, fh1.getRouteDistance(locationFH1, poi));
 
         Vector<Double> myWeights = new Vector<>();
         myWeights.add(0.25);
@@ -36,15 +38,43 @@ public class FieldHospital {
         System.out.println("The route score for the given weight values is: " + fH1WeightedDesirablity);
 
         LinkedList<FieldHospital> myFieldHospitals = new LinkedList<>();
-        FieldHospital fhA = new FieldHospital(factors);
-        FieldHospital fhB = new FieldHospital(factors);
-        FieldHospital fhC = new FieldHospital(factors);
+        Pinpoint locationA = new Pinpoint(0,9);
+        LinkedList<Double> factorsA = new LinkedList<>();
+        FieldHospital fhA = new FieldHospital(factorsA, locationA, poi); //TODO is this getting the wrong location for factors? (see line 33)
+        fhA.setFHName("The A FH");
+        factorsA.add(0, fhA.getDanger());
+        factorsA.add(1, fhA.getUnavailability());
+        factorsA.add(2, fhA.getRouteDistance(locationA, poi));
+
+        LinkedList<Double> factorsB = new LinkedList<>();
+        Pinpoint locationB = new Pinpoint(5,11);
+        FieldHospital fhB = new FieldHospital(factorsB, locationB, poi);
+        fhB.setFHName("The B FH");
+        factorsB.add(0, fhB.getDanger());
+        factorsB.add(1, fhB.getUnavailability());
+        factorsB.add(2, fhB.getRouteDistance(locationB, poi));
+
+        LinkedList<Double> factorsC = new LinkedList<>();
+        Pinpoint locationC = new Pinpoint(3,6);
+        FieldHospital fhC = new FieldHospital(factorsC, locationC, poi);
+        fhC.setFHName("The C FH");
+        factorsC.add(0, fhC.getDanger());
+        factorsC.add(1, fhC.getUnavailability());
+        factorsC.add(2, fhC.getRouteDistance(locationC, poi));
+
+        LinkedList<Double> factorsD = new LinkedList<>();
+        Pinpoint locationD = new Pinpoint(7,0);
+        FieldHospital fhD = new FieldHospital(factorsD, locationD, poi);
+        fhD.setFHName("The D FH");
+        factorsD.add(0, fhD.getDanger());
+        factorsD.add(1, fhD.getUnavailability());
+        factorsD.add(2, fhD.getRouteDistance(locationD, poi));
+
         myFieldHospitals.add(fhA);
         myFieldHospitals.add(fhB);
         myFieldHospitals.add(fhC);
-        //TODO: change poi to have type of coordinates and make getDistance take in poi for calculation of euclidean
-        //TODO: give FieldHospitals coordinates of their own locations
-        poi = 0.6;
+        myFieldHospitals.add(fhD);
+
         top3FieldHospitalsCalculator(myFieldHospitals, poi);
 
     }
@@ -53,11 +83,13 @@ public class FieldHospital {
     //input: weights, route
     //output: weightedSum
     //for a given route, do factor methods times each weight and add up
-    public static double weightedSumCalculator(Vector<Double> weights, FieldHospital myRoute) {
+    public static double weightedSumCalculator(Vector<Double> weights, FieldHospital myFH) {
         double weightedSum = 0;
         for (int i = 0; i < weights.size(); i++) {
             if (weights.get(i) != 0.0) {
-                weightedSum += weights.get(i) * myRoute.factors.get(i);
+                weightedSum += weights.get(i) * myFH.factors.get(i); //TODO factors need to figure out how to make
+                                                                        // it get the right factor for locations
+                                                                        // a, b, c, d
             }
         }
         return weightedSum;
@@ -66,13 +98,13 @@ public class FieldHospital {
     //input: list of field hospitals, poi
     //output: list of best 3 field hospitals
     //calculate weighted desirability with respect to point of injury
-    public static ArrayList<FieldHospital> top3FieldHospitalsCalculator(LinkedList<FieldHospital> fieldHospitals, double poi) {
+    public static ArrayList<String> top3FieldHospitalsCalculator(LinkedList<FieldHospital> fieldHospitals, Pinpoint poi) {
         Vector<Double> myWeights = new Vector<>();
         myWeights.add(0.25);
         myWeights.add(0.50);
         myWeights.add(0.75);
         HashMap<FieldHospital, Double> allFHWeightedSums = new HashMap<FieldHospital, Double>();
-        ArrayList<FieldHospital> top3FH = new ArrayList<>();
+        ArrayList<String> top3FH = new ArrayList<>();
         double first, second, third;
 
         for (FieldHospital fh : fieldHospitals) {
@@ -93,40 +125,35 @@ public class FieldHospital {
               then update first, second, and third */
                 third = second = first;
                 first = entry.getValue();
-                top3FH.add(entry.getKey());
+                top3FH.add(entry.getKey().getFHName());
                 /* If allFHWeightedSums.get(i) is in between first and second
                then update second  */
             } else if (entry.getValue() < second && entry.getValue() != first) {
                 second = entry.getValue();
-                top3FH.add(entry.getKey());
+                top3FH.add(entry.getKey().getFHName());
                 /* If allFHWeightedSums.get(i) is in between second and third
                then update third  */
             } else {
-                if (entry.getValue() < third && entry.getValue() != second) {
+                if (entry.getValue() < third && entry.getValue() != second && entry.getValue() != first) {
                     third = entry.getValue();
-                    top3FH.add(entry.getKey());
+                    top3FH.add(entry.getKey().getFHName());
                 }
             }
-            if (third == Integer.MAX_VALUE) {
-                System.out.println("There is no third" +
-                        "smallest element");
-            } else if (second == Integer.MAX_VALUE) {
-                System.out.println("There is no second" +
-                        "smallest element");
-            } else {
-                System.out.println("The smallest element is " + first + " and second Smallest" +
-                        " element is " + second + " and third Smallest" +
-                        " element is " + third);
-
-            }
         }
-        //TODO
-        //i need to figure out how to get the key associated with the values first, second, and third to put them in the
-        //top3FH linked list
-        //should I switch to an arraylist? or some other version of a hashmap?
-        return top3FH;
 
+        if (third == Integer.MAX_VALUE) {
+            System.out.println("There is no third smallest element");
+        } else if (second == Integer.MAX_VALUE) {
+            System.out.println("There is no second smallest element");
+        } else {
+            System.out.println("The smallest element is " + first + " and second Smallest" +
+                    " element is " + second + " and third Smallest" +
+                    " element is " + third);
+        }
+
+        return top3FH;
     }
+
 
     //getter for field hospital name
     public String getFHName(){
@@ -138,6 +165,16 @@ public class FieldHospital {
         this.fHName = fHName;
     }
 
+    //getter for field hospital location/coordinates
+    public Pinpoint getFHLocation(){
+        return this.fHLocation;
+    }
+
+    //setter for field hospital location/coordinates
+    public void setfHLocation(Pinpoint fHLocation){
+        this.fHLocation = fHLocation;
+    }
+
     public double getDanger() {
         return this.danger = 0.8;
     }
@@ -146,7 +183,7 @@ public class FieldHospital {
         return this.unavailability = 0.8;
     }
 
-    public double getRouteDistance() {
-        return this.routeDistance = 0.8;
+    public double getRouteDistance(Pinpoint fHLocation, Pinpoint poi) {
+        return Pinpoint.eDistance(fHLocation, poi);
     }
 }
--- src/TestFieldHospital.java
+++ src/TestFieldHospital.java
@@ -1,130 +1,219 @@
 import org.junit.jupiter.api.Test;
 
 import java.util.LinkedList;
+import java.util.List;
+import static java.util.Arrays.asList;
 import java.util.Vector;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TestFieldHospital {
 
+    //Testing top3FHCalculator
+    @Test
+    void testTop3FHJustWorks(){
+
+        Pinpoint poi = new Pinpoint(0,0);
+        LinkedList<FieldHospital> myFieldHospitals = new LinkedList<>();
+        Pinpoint locationA = new Pinpoint(0,9);
+        LinkedList<Double> factorsA = new LinkedList<>();
+        FieldHospital fhA = new FieldHospital(factorsA, locationA, poi); //TODO is this getting the wrong location for factors? (see line 33)
+        fhA.setFHName("The A FH");
+        factorsA.add(0, fhA.getDanger());
+        factorsA.add(1, fhA.getUnavailability());
+        factorsA.add(2, fhA.getRouteDistance(locationA, poi));
+
+        LinkedList<Double> factorsB = new LinkedList<>();
+        Pinpoint locationB = new Pinpoint(5,11);
+        FieldHospital fhB = new FieldHospital(factorsB, locationB, poi);
+        fhB.setFHName("The B FH");
+        factorsA.add(0, fhB.getDanger());
+        factorsA.add(1, fhB.getUnavailability());
+        factorsA.add(2, fhB.getRouteDistance(locationB, poi));
+
+        LinkedList<Double> factorsC = new LinkedList<>();
+        Pinpoint locationC = new Pinpoint(3,6);
+        FieldHospital fhC = new FieldHospital(factorsC, locationC, poi);
+        fhC.setFHName("The C FH");
+        factorsC.add(0, fhC.getDanger());
+        factorsC.add(1, fhC.getUnavailability());
+        factorsC.add(2, fhC.getRouteDistance(locationC, poi));
+
+        LinkedList<Double> factorsD = new LinkedList<>();
+        Pinpoint locationD = new Pinpoint(7,0);
+        FieldHospital fhD = new FieldHospital(factorsD, locationD, poi);
+        fhD.setFHName("The D FH");
+        factorsD.add(0, fhD.getDanger());
+        factorsD.add(1, fhD.getUnavailability());
+        factorsD.add(2, fhD.getRouteDistance(locationD, poi));
+
+        myFieldHospitals.add(fhA);
+        myFieldHospitals.add(fhB);
+        myFieldHospitals.add(fhC);
+        myFieldHospitals.add(fhD);
+
+        List<String> bestFHs = asList("The C FH", "The B FH", "The A FH");
+        assertTrue(FieldHospital.top3FieldHospitalsCalculator(myFieldHospitals, poi).equals(bestFHs));
+    }
+
+
+
+    //Testing WeightedSumCalculator
     @Test
     void testWeightedSumThreeWeights() {
 
         FieldHospital.factors = new LinkedList<>();
-        FieldHospital myFH = new FieldHospital(FieldHospital.factors);
+        Pinpoint locationMyFH = new Pinpoint(0,9);
+        Pinpoint poi = new Pinpoint(0,0);
+        FieldHospital myFH = new FieldHospital(FieldHospital.factors, locationMyFH, poi);
         FieldHospital.factors.add(0, myFH.getDanger());
         FieldHospital.factors.add(1, myFH.getUnavailability());
-        FieldHospital.factors.add(2, myFH.getRouteDistance());
+        FieldHospital.factors.add(2, myFH.getRouteDistance(poi, locationMyFH));
+
         Vector<Double> myWeights = new Vector<>();
         myWeights.add(0.25);
         myWeights.add(0.50);
         myWeights.add(0.75);
+
         assertEquals(1.2000000000000002, FieldHospital.weightedSumCalculator(myWeights, myFH));
     }
 
     @Test
     void testWeightedSumThreeWeightsFirstOneZero() {
         FieldHospital.factors = new LinkedList<>();
-        FieldHospital myFH = new FieldHospital(FieldHospital.factors);
+        Pinpoint locationMyFH = new Pinpoint(0,9);
+        Pinpoint poi = new Pinpoint(0,0);
+        FieldHospital myFH = new FieldHospital(FieldHospital.factors, locationMyFH, poi);
         FieldHospital.factors.add(0, myFH.getDanger());
         FieldHospital.factors.add(1, myFH.getUnavailability());
-        FieldHospital.factors.add(2, myFH.getRouteDistance());
+        FieldHospital.factors.add(2, myFH.getRouteDistance(poi, locationMyFH));
 
         Vector<Double> myWeights = new Vector<>();
         myWeights.add(0.0);
         myWeights.add(1.0);
         myWeights.add(1.0);
+
         assertEquals(1.6, FieldHospital.weightedSumCalculator(myWeights, myFH));
     }
 
     @Test
     void testWeightedSumThreeWeightsSecondOneZero() {
         FieldHospital.factors = new LinkedList<>();
-        FieldHospital myFH = new FieldHospital(FieldHospital.factors);
+        Pinpoint locationMyFH = new Pinpoint(0,9);
+        Pinpoint poi = new Pinpoint(0,0);
+        FieldHospital myFH = new FieldHospital(FieldHospital.factors, locationMyFH, poi);
         FieldHospital.factors.add(0, myFH.getDanger());
         FieldHospital.factors.add(1, myFH.getUnavailability());
-        FieldHospital.factors.add(2, myFH.getRouteDistance());
+        FieldHospital.factors.add(2, myFH.getRouteDistance(poi, locationMyFH));
+
         Vector<Double> myWeights = new Vector<>();
         myWeights.add(0.25);
         myWeights.add(0.0);
         myWeights.add(0.75);
+
         assertEquals(0.8, FieldHospital.weightedSumCalculator(myWeights, myFH));
     }
 
     @Test
     void testWeightedSumThreeWeightsThirdOneZero() {
         FieldHospital.factors = new LinkedList<>();
-        FieldHospital myFH = new FieldHospital(FieldHospital.factors);
+        Pinpoint locationMyFH = new Pinpoint(0,9);
+        Pinpoint poi = new Pinpoint(0,0);
+        FieldHospital myFH = new FieldHospital(FieldHospital.factors, locationMyFH, poi);
         FieldHospital.factors.add(0, myFH.getDanger());
         FieldHospital.factors.add(1, myFH.getUnavailability());
-        FieldHospital.factors.add(2, myFH.getRouteDistance());
+        FieldHospital.factors.add(2, myFH.getRouteDistance(poi, locationMyFH));
+
         Vector<Double> myWeights = new Vector<>();
         myWeights.add(0.25);
         myWeights.add(0.50);
         myWeights.add(0.0);
+
         assertEquals(0.6000000000000001, FieldHospital.weightedSumCalculator(myWeights, myFH));
     }
 
     @Test
     void testWeightedSumTwoWeights() {
         FieldHospital.factors = new LinkedList<>();
-        FieldHospital myFH = new FieldHospital(FieldHospital.factors);
+        Pinpoint locationMyFH = new Pinpoint(0,9);
+        Pinpoint poi = new Pinpoint(0,0);
+        FieldHospital myFH = new FieldHospital(FieldHospital.factors, locationMyFH, poi);
         FieldHospital.factors.add(0, myFH.getDanger());
         FieldHospital.factors.add(1, myFH.getUnavailability());
-        FieldHospital.factors.add(2, myFH.getRouteDistance());
+        FieldHospital.factors.add(2, myFH.getRouteDistance(poi, locationMyFH));
+
         Vector<Double> myWeights = new Vector<>();
         myWeights.add(0.25);
         myWeights.add(0.50);
+
         assertEquals(0.6000000000000001, FieldHospital.weightedSumCalculator(myWeights, myFH));
     }
 
     @Test
     void testWeightedSumTwoWeightsFirstZero() {
         FieldHospital.factors = new LinkedList<>();
-        FieldHospital myFH = new FieldHospital(FieldHospital.factors);
+        Pinpoint locationMyFH = new Pinpoint(0,9);
+        Pinpoint poi = new Pinpoint(0,0);
+        FieldHospital myFH = new FieldHospital(FieldHospital.factors, locationMyFH, poi);
         FieldHospital.factors.add(0, myFH.getDanger());
         FieldHospital.factors.add(1, myFH.getUnavailability());
-        FieldHospital.factors.add(2, myFH.getRouteDistance());
+        FieldHospital.factors.add(2, myFH.getRouteDistance(poi, locationMyFH));
+
         Vector<Double> myWeights = new Vector<>();
         myWeights.add(0.0);
         myWeights.add(0.50);
+
         assertEquals(0.4, FieldHospital.weightedSumCalculator(myWeights, myFH));
     }
 
     @Test
     void testWeightedSumTwoWeightsSecondZero() {
         FieldHospital.factors = new LinkedList<>();
-        FieldHospital myFH = new FieldHospital(FieldHospital.factors);
+        Pinpoint locationMyFH = new Pinpoint(0,9);
+        Pinpoint poi = new Pinpoint(0,0);
+        FieldHospital myFH = new FieldHospital(FieldHospital.factors, locationMyFH, poi);
         FieldHospital.factors.add(0, myFH.getDanger());
         FieldHospital.factors.add(1, myFH.getUnavailability());
-        FieldHospital.factors.add(2, myFH.getRouteDistance());
+        FieldHospital.factors.add(2, myFH.getRouteDistance(poi, locationMyFH));
+
         Vector<Double> myWeights = new Vector<>();
         myWeights.add(0.25);
         myWeights.add(0.0);
+
         assertEquals(0.2, FieldHospital.weightedSumCalculator(myWeights, myFH));
     }
 
     @Test
     void testWeightedSumOneWeight() {
         FieldHospital.factors = new LinkedList<>();
-        FieldHospital myFH = new FieldHospital(FieldHospital.factors);
+        Pinpoint locationMyFH = new Pinpoint(0,9);
+        Pinpoint poi = new Pinpoint(0,0);
+        FieldHospital myFH = new FieldHospital(FieldHospital.factors, locationMyFH, poi);
+
         FieldHospital.factors.add(0, myFH.getDanger());
         FieldHospital.factors.add(1, myFH.getUnavailability());
-        FieldHospital.factors.add(2, myFH.getRouteDistance());
+        FieldHospital.factors.add(2, myFH.getRouteDistance(poi, locationMyFH));
+
         Vector<Double> myWeights = new Vector<>();
         myWeights.add(0.25);
+
         assertEquals(0.2, FieldHospital.weightedSumCalculator(myWeights, myFH));
     }
 
     @Test
     void testWeightedSumOneWeightZeroWeightValues() {
         FieldHospital.factors = new LinkedList<>();
-        FieldHospital myFH = new FieldHospital(FieldHospital.factors);
+        Pinpoint locationMyFH = new Pinpoint(0,9);
+        Pinpoint poi = new Pinpoint(0,0);
+        FieldHospital myFH = new FieldHospital(FieldHospital.factors, locationMyFH, poi);
+
         FieldHospital.factors.add(0, myFH.getDanger());
         FieldHospital.factors.add(1, myFH.getUnavailability());
-        FieldHospital.factors.add(2, myFH.getRouteDistance());
+        FieldHospital.factors.add(2, myFH.getRouteDistance(poi, locationMyFH));
+
         Vector<Double> myWeights = new Vector<>();
         myWeights.add(0.0);
+
         assertEquals(0.0, FieldHospital.weightedSumCalculator(myWeights, myFH));
     }
 
