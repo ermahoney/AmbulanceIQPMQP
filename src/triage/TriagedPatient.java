63268ae2c65c5bbe3bad86da01284ac441eb7681
Alexandria Miera <mariebotic@gmail.com>
2021-07-11 22:39:01 -0400

    Fixes #11
    Fixes #11 by adjusted the code by changing the name of the weighted calculator function to calculateRetreivableWelfare and adjusting the logic of the getExtractionTime function to simply return some arbitrary value. The glossary was also updated to fit to the definitions mentioned in the TriagedPatient class.

--- src/triage/TriagedPatient.java
+++ src/triage/TriagedPatient.java
@@ -1,77 +1,72 @@
-// Authors: Emily Mahoney, Alex Miera
-// Creation date: June 5, 2021
-// Last edited: June 5, 2021
+/* Authors: Emily Mahoney, Alex Miera */
+/* Creation date: July 11, 2021 */
+/* Last edited: July 11, 2021 */
 
 import java.util.LinkedList;
 import java.util.Vector;
 
 public class TriagedPatient {
-    public static double retrievableWelfare; // Declares the factor that will account for extraction time during triage
-    public static double extractionTime; // Declares the time it will take to extract the patient
-    public static LinkedList<Double> factors; // Declares factors that make up triaged patient object
+    /* Declares variables that make up retrievable welfare */
+    public static double extractionTime; /* Declares the time it will take to 
+                                            extract the patient */
 
-    // Declares triaged patient object
+    public static LinkedList<Double> factors; /* Declares factors that make up 
+                                                 triaged patient object */
+
+    /* Declares triaged patient object */
     public TriagedPatient(LinkedList<Double> factors) {
-        this.retrievableWelfare = getRetrievableWelfare(extractionTime);
+        this.extractionTime = getExtractionTime()
     }
 
     public static void main (String[] args) {
-        // Defines extraction time
-        // Note: extraction time may need to be interrelated with Route.getRouteDistance();
+        /* Defines variables that make up retrivable welfare at runtime */
         extractionTime = getExtractionTime();
 
-        // Defines a triaged patient
+        /* Defines a triaged patient and adds factors that will make up 
+           retrievable welfare */
         factors = new LinkedList<>();
         TriagedPatient patient1 = new TriagedPatient(factors);
-        factors.add(0, patient1.getRetrievableWelfare(extractionTime));
+        factors.add(0, patient1.getExtractionTime());
 
-        // Defines weights for each factor
-        Vector<Double> myWeights = new Vector<>();
-        myWeights.add(0.25);
+        /* Defines a weight for each factor */
+        Vector<Double> weights = new Vector<>();
+        weights.add(0.25);
 
-        // Tests weighted sum calculator
-        double triageWeight = weightedSumCalculator(myWeights, patient1);
-        System.out.println("The patient triage weight for the given weight values is: " + triageWeight);
+        /* Tests weighted sum caclulator */
+        double retrievableWelfare = 
+            calculateRetrievableWelfare(weights, patient1);
+        System.out.println("The patient triage weight for the given weight values is: " + retrievableWelfare);
     }
 
-    // Note: number of factors is specified at runtime
-    // Input: weights, triaged patient
-    /* Output: triage weight
+    /* Function that calculates the patients likelyhood of survival, i.e. 
+       retrievable welfare
+     * 
+     * Note: number of factors is specified at runtime
+     * 
+     * Input: weights, triaged patient
+     * Output: triage weight
                low triage weight = green to yellow tag
                hight triage weight = red to black tag
     */
-    public static double weightedSumCalculator( Vector<Double> weights, TriagedPatient myTriagedPatient){
+    public static double calculateRetrievableWelfare(Vector<Double> weights, TriagedPatient triagedPatient){
         double weightedSum = 0;
 
         for( int i = 0; i < weights.size(); i++ ){
             if( weights.get(i) != 0.0){
-                weightedSum += weights.get(i) * myTriagedPatient.factors.get(i);
+                weightedSum += weights.get(i) * triagedPatient.factors.get(i);
             }
         }
         return weightedSum;
     }
 
-    // Input: extraction time or the time it takes to get to the patient
-    /* Output: retrievable welfare
-               low retrievable welfare = positive patient outlook
-               high retrievable welfare = negative patient outlook
+    /* Function that gets the time it takes to transfer the patient from the
+     * ground to the field hospital starting at the time of injury (t_i) 
+     * and ending at the time of arrival at a field hospital (s_rh)
+     * 
+     * Input: void
+     * Output: extraction time in min
     */
-    public double getRetrievableWelfare(double extractionTime) {
-        // Creates an array of 60 values according to the golden hour
-        double time[] = new double[60];
-        for (int i = 0; i < time.length; i++) {
-            time[i] = i + 1;
-        }
-
-        // Maxes out retrievable welfare to golen hour limit
-        if (extractionTime > 60) {
-            return this.retrievableWelfare = 60;
-        }
-
-        return this.retrievableWelfare = extractionTime;
-    }
-
     public static double getExtractionTime() {
-        return 10;
+        return 70;
     }
 }
