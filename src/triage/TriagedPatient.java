2e5c6d6e276be9dc065ba0c037620dc5acaac1c8
Alexandria Miera <mariebotic@gmail.com>
2021-07-12 20:52:10 -0400

    Fixes #9
    Fixes #9 by creating factor functions according to pg. 2159 and Section 2 Chaptor 9 of Emergency Care and Transportation of the Sick and Injured texbook.

--- src/triage/TriagedPatient.java
+++ src/triage/TriagedPatient.java
@@ -9,13 +9,29 @@ public class TriagedPatient {
     /* Declares variables that make up retrievable welfare */
     public static double extractionTime; /* Declares the time it will take to 
                                             extract the patient */
+    public static double burnPercent;
+    public static double respirationRate;
+    public static double tidalVolume;
+    public static double pulse;
+    public static double systolicPressure;
+    public static double diastolicPressure;
+    public static double temperature;
+    public static double bloodOxygen;
 
     public static LinkedList<Double> factors; /* Declares factors that make up 
                                                  triaged patient object */
 
     /* Declares triaged patient object */
     public TriagedPatient(LinkedList<Double> factors) {
-        this.extractionTime = getExtractionTime()
+        this.extractionTime = getExtractionTime();
+        this.burnPercent = getBurnPercent();
+        this.respirationRate = getRespirationRate();
+        this.tidalVolume = getTidalVolume();
+        this.pulse = getPulse();
+        this.systolicPressure = getSystolicPressure();
+        this.diastolicPressure = getDiastolicPressure();
+        this.temperature = getTemperature();
+        this.bloodOxygen = getBloodOxygen();
     }
 
     public static void main (String[] args) {
@@ -25,16 +41,32 @@ public class TriagedPatient {
         /* Defines a triaged patient and adds factors that will make up 
            retrievable welfare */
         factors = new LinkedList<>();
-        TriagedPatient patient1 = new TriagedPatient(factors);
-        factors.add(0, patient1.getExtractionTime());
+        TriagedPatient patient = new TriagedPatient(factors);
+        factors.add(0, patient.getExtractionTime());
+        factors.add(1, patient.getBurnPercent());
+        factors.add(2, patient.getRespirationRate());
+        factors.add(3, patient.getTidalVolume());
+        factors.add(4, patient.getPulse());
+        factors.add(5, patient.getSystolicPressure());
+        factors.add(6, patient.getDiastolicPressure());
+        factors.add(7, patient.getTemperature());
+        factors.add(8, patient.getBloodOxygen());
 
         /* Defines a weight for each factor */
         Vector<Double> weights = new Vector<>();
-        weights.add(0.25);
+        weights.add(0.4);
+        weights.add(0.5);
+        weights.add(0.15);
+        weights.add(0.2);
+        weights.add(0.3);
+        weights.add(0.2);
+        weights.add(0.1);
+        weights.add(0.05);
+        weights.add(0.35);
 
         /* Tests weighted sum caclulator */
         double retrievableWelfare = 
-            calculateRetrievableWelfare(weights, patient1);
+            calculateRetrievableWelfare(weights, patient);
         System.out.println("The patient triage weight for the given weight values is: " + retrievableWelfare);
     }
 
@@ -60,13 +92,46 @@ public class TriagedPatient {
     }
 
     /* Function that gets the time it takes to transfer the patient from the
-     * ground to the field hospital starting at the time of injury (t_i) 
-     * and ending at the time of arrival at a field hospital (s_rh)
+     * ground to the field hospital starting at the time a distress signal
+     * was sent to EMS (t_r) and ending at the time of arrival at a field 
+     * hospital (s_rh)
      * 
      * Input: void
      * Output: extraction time in min
     */
     public static double getExtractionTime() {
         return 70;
+    }
+
+    public static double getBurnPercent() {
+        return 90.9;
+    }
+
+    public static double getRespirationRate() {
+        return 20;
+    }
+
+    public static double getTidalVolume() {
+        return 8;
+    }
+
+    public static double getPulse() {
+        return 70;
+    }
+
+    public static double getSystolicPressure() {
+        return 115;
+    }
+
+    public static double getDiastolicPressure() {
+        return 70;
+    }
+
+    public static double getTemperature() {
+        return 99.5;
+    }
+
+    public static double getBloodOxygen() {
+        return 99;
     }
 }
