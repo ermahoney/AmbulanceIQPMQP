3b47821f528b2ac9ee65627c2ed1a15a896ac36c
Patrick C <xeru98@gmail.com>
2021-09-24 12:33:01 -0400

    Rewrite of linked list

--- src/common/linkedlist.cpp
+++ src/common/linkedlist.cpp
@@ -1,326 +1,148 @@
 /** File for implementation of linked list
  *
- * @author Blake Nelson, Alex Miera
+ * @author Blake Nelson, Alex Miera, Patrick Critz
  *
  */
 
-#include <stdlib.h>
-#include <string.h>
+#include <cstdlib>
+#include <cstring>
 #include "linkedlist.hpp"
 
-#define TRUE (1)
-#define FALSE (0)
 
-
-
-/** Function to create an empty linked list
- *
- * @return pointer to linked list structure or NULL on error
+/** Default Constructor
  */
-LinkedList::LLIST* LinkedList::create (void) {
-  LLIST *pll = (LLIST*) malloc(sizeof(LLIST));
-  if (pll != NULL) {
-    pll->head = NULL;
-    pll->tail = NULL;
-  }
-  return pll;
+template<typename T>
+LinkedList<T>::LinkedList(){
+    head = nullptr;
+    tail = nullptr;
+    num_elements = 0;
 }
 
-
-/** Function to destroy a linked list. Linked list must be empty
- *
- * @param pll is a pointer to the linked list
- * @return zero on success, nonzero on failure
+/** Default Destructor
+ * Assumes that the list is still perfectly forwardly linked
  */
-int destroy (LinkedList::LLIST* pll) {
-  if ((pll->head != NULL) || (pll->tail != NULL)) {
-    // If linked list is not empty, something is wrong
-    return -1;
-  } else {
-    // liked list is empty, release metadata struct
-    free (pll);
-  }
-  return 0;
-}
-
-
-
-/** Function to get first node in llist for iteration
- *
- * @param pll is a pointer to the linked list
- * @return first node in the llist or null if empty
- */
-LinkedList::LLNODE* LinkedList::get_first_node (LinkedList::LLIST *pll) {
-  return pll->head;
-}
-
-
-
-/** Function to get the last node in llist for iteration
- *
- * @param pll is a pointer to the linked list
- * @return first node in the llist or null if empty
- */
-LinkedList::LLNODE* LinkedList::get_last_node (LinkedList::LLIST* pll) {
-  return pll->tail;
-}
-
-
-
-/** Function to get the next node in llist during iteration
- *
- * @param pnode is a pointer to the current node during iteration
- * @return the next node (node following pnode) or NULL
- */
-LinkedList::LLNODE* LinkedList::get_next_node (LinkedList::LLNODE* pnode) {
-  return pnode->next;
-}
-
-
-
-/** Function to get the previous node in llist during iteration
- *
- * @param pnode is a pointer to the current node during iteration
- * @return the prev node (node preceding pnode) or NULL
- */
-LinkedList::LLNODE* LinkedList::get_prev_node (LinkedList::LLNODE* pnode) {
-  return pnode->prev;
-}
-
-
-
-/** Function to add node to head of list
- *
- * @param pll is a pointer to the linked list
- * @param pdata is a pointer to the data
- * @return zero for success or nonzero for failure
- */
-int LinkedList::addhead (LinkedList::LLIST* pll, void* pdata) {
-  LinkedList::LLNODE* pnode; /* pointer to the new node */
-
-  pnode = (LinkedList::LLNODE*) calloc(1, sizeof(LinkedList::LLNODE));
-  if (pnode == 0) {
-    return -1; /* failed allocation, no change to llist */
-  }
-
-  /* link the data to the node just allocated */
-  pnode->data = pdata;
-
-  /* Link the new node to the head of the linked list */
-  if (pll->head == NULL) {
-    /* this is the only node in the list, nothing before nothing after */
-    pnode->prev = NULL;
-    pll->head = pnode;
-    pnode->next = NULL;
-    pll->tail = pnode;
-  } else {
-    /* link this into the list at the head position */
-    pnode->prev = NULL;
-    pnode->next = pll->head;
-    pll->head->prev = pnode;
-    pll->head = pnode;
-  }
-
-  return 0;
-}
-
-
-
-/** Function to add node to tail of list
- *
- * @param pll is a pointer to the linked list
- * @param pdata is a pointer to the data
- * @return zero for success or nonzero for failure
- */
-int LinkedList::addtail (LinkedList::LLIST* pll, void* pdata) {
-  LinkedList::LLNODE *pnode; /* pointer to the new node */
-
-  pnode = (LinkedList::LLNODE *) calloc(1, sizeof(LinkedList::LLNODE));
-  if (pnode == 0) {
-    return -1; /* failed allocation, no change to llist */
-  }
-
-  /* link the data to the node just allocated */
-  pnode->data = pdata;
-
-  /* link the new node to the tail of the linked list */
-  if (pll->tail == NULL) {
-    /* this is the only node in the list, nothing before nor following */
-    pnode->prev = NULL;
-    pll->head = pnode;
-    pnode->next = NULL;
-    pll->tail = pnode;
-  } else {
-    /* link this node into the list at the tail position
-     */
-    pnode->next = NULL;
-    pnode->prev = pll->tail;
-    pll->tail->next = pnode;
-    pll->tail = pnode;
-  }
-
-  return 0;
-}
-
-
-
-/** Function to delete specified node from the list
- *
- * Note: This function will release the node back to the system
- *       (the memory allocated for the linked list node) if the
- *       specified node is found in the list. The client (calling)
- *       code is responsible for releasing the memory of the data
- *       the node points at. This frees the memory for the node but
- *       not the data the node points to.
- *
- * @param pll is a pointer to the linked list
- * @param pnode is pointer to node to delete
- * @return zero for success, nonzero on error
- */
-int LinkedList::deletenode (LinkedList::LLIST* pll, LinkedList::LLNODE* pnode) {
-  LinkedList::LLNODE *pcur;     // current node as we iterate the list
-
-  /* walk the list until we find the wanted node. Error if not we don't find it */
-  pcur = pll->head;
-  while (pcur && pcur != pnode) {
-    pcur = pcur->next;
-  }
-  if (!pcur) {
-    return -1;
-  }
-
-  /* either at node we want to delete, or end of the list
-   * special processing for head and tail positions
-   */
-  if (pcur) {
-    /* unlink from the previous node */
-    if (pll->head == pcur) {
-      /* special processing if removing head of list */
-      pll->head = pcur->next;
-    } else {
-      pcur->prev->next = pcur->next;
+template<typename T>
+LinkedList<T>::~LinkedList() {
+    LinkedList::Node* node = head;
+    while(node){
+        LinkedList::Node* next = node->next;
+        delete node;
+        node = next;
+        num_elements -= 1;
     }
-
-    /* unlink from the following node */
-    if (pll->tail == pcur) {
-      /* special processing if removing tail of list */
-      pll->tail = pcur->prev;
-    } else {
-      pcur->next->prev = pcur->prev;
-    }
-
-    /* free the node
-     * client (calling) code responsible for releasing the data node points at
-     */
-    free (pcur);
-  }
-
-  /* return success */
-  return 0;
+    //TODO add custom error checking for full cleared list
+    delete this;
 }
 
-
-
-/** Function to add a node before the specified node (pnode)
- *
- * @param pll is a pointer to the linked list
- * @param pnode is a pointer to the node to add before
- * @param pdata is a pointer to the data
- * @return zero if success or nonzero on error
- */
-int LinkedList::add_before (LinkedList::LLIST* pll, LinkedList::LLNODE* pnode, void* pdata) {
-  LinkedList::LLNODE* pnewnode = NULL;
-  pnewnode = (LinkedList::LLNODE *) calloc (1, sizeof(LinkedList::LLNODE));
-  if (pnewnode == NULL) {
-    /* failed to allocate memory for new node */
-    return -1;
-  }
-
-  /* link the data to the node then link the node into the list */
-  pnewnode->data = pdata;
-
-  /* special processing if we are adding to empty list
-   * verify parameters are sensible for this situation
-   */
-  if ((pll->head == NULL) || (pll->tail == NULL)) {
-    /* Head, tail and pnode must all be null or something's wrong */
-    if (! ((pll->head == NULL) && (pll->tail == NULL) && (pnode == NULL))) {
-      free (pnewnode);
-      return -2;
+template<typename T>
+typename LinkedList<T>::Node* LinkedList<T>::push_front(T data) {
+    LinkedList<T>::Node* n = new LinkedList<T>::Node();
+    n->next = head;
+    head = n;
+    if(!tail){ //if the tail is empty this the list must have started empty
+        tail = n;
     }
+    num_elements += 1;
+    return n;
+}
 
-    /* adding into an empty list - this is the one and only node */
-    pnewnode->prev = pnewnode->next = NULL;
-    pll->head = pll->tail = pnewnode;
+template<typename T>
+typename LinkedList<T>::Node* LinkedList<T>::push_back(T data) {
+    LinkedList<T>::Node* n = new LinkedList<T>::Node();
+    n->prev = tail;
+    tail = n;
+    if(!head){ //if the tail is empty this the list must have started empty
+        head = n;
+    }
+    num_elements += 1;
+    return n;
+}
+
+template<typename T>
+T LinkedList<T>::pop_front() {
+    LinkedList<T>::Node* n = head;
+    T* v = n->data;
+    head = n->next;
+    delete n;
+    num_elements -= 1;
+    return v;
+}
+
+template<typename T>
+T LinkedList<T>::pop_back() {
+    LinkedList<T>::Node* n = tail;
+    T* v = n->data;
+    tail = n->next;
+    delete n;
+    num_elements -= 1;
+    return v;
+}
+
+template<typename T>
+typename LinkedList<T>::Node* LinkedList<T>::get_head() {
+    return head;
+}
+
+template<typename T>
+typename LinkedList<T>::Node* LinkedList<T>::get_tail() {
+    return tail;
+}
+
+template<typename T>
+typename LinkedList<T>::Node* LinkedList<T>::insert_before(LinkedList::Node* before, T data) {
+    if(!before){
+        return nullptr;
+    }
+    LinkedList<T>::Node* n = new LinkedList<T>::Node();
+    n->data = data;
+    n->prev = before->prev;
+    n->next = before;
+    before->prev = n;
+    if(head == before) { //replace the head if adding before the current head
+        head = n;
+    }
+    num_elements += 1;
+    return n;
+}
+
+template<typename T>
+typename LinkedList<T>::Node* LinkedList<T>::insert_after(LinkedList::Node* after, T data) {
+    if(!after){
+        return nullptr;
+    }
+    LinkedList<T>::Node* n = new LinkedList<T>::Node();
+    n->data = data;
+    n->prev = after;
+    n->next = after->next;
+    after->next = n;
+    if(tail == after) { //replace the tail if adding after the tail
+        tail = n;
+    }
+    num_elements += 1;
+    return n;
+}
+
+template<typename T>
+int LinkedList<T>::remove_node(LinkedList::Node* node_to_remove) {
+    if(!node_to_remove){
+        return -2;
+    }
+    LinkedList<T>::Node* n = head;
+    while(n && n != node_to_remove){
+        n = n->next;
+        if(n == tail){
+            return -1;
+        }
+    }
+    //reconnect prev and next
+    n->prev->next = n->next;
+    n->next->prev = n->prev;
+    delete n;
+    num_elements -= 1;
     return 0;
-  }
-
-  /* we are adding before the current node into a non empty list
-   * possibly the head of the list
-   */
-  pnewnode->next = pnode;
-  pnewnode->prev = pnode->prev;
-  pnode->prev = pnewnode;
-  if (pll->head == pnode) {
-    /* adding to head of list */
-    pll->head = pnewnode;
-  } else {
-    /* not at head of the list */
-    pnewnode->prev->next = pnewnode;
-  }
-
-  return 0;
 }
 
-
-
-/** Function to add a node after the current 
- *
- * @param pll is a pointer to the linked list
- * @param pnode is a pointer to the not to add after
- * @param pdata is a pointer to the data
- * @return zero if success or nonzero on error
- */
-int LinkedList::add_after (LinkedList::LLIST* pll, LinkedList::LLNODE* pnode, void* pdata) {
-  LinkedList::LLNODE *pnewnode = NULL;
-  pnewnode = (LinkedList::LLNODE *) calloc (1, sizeof(LinkedList::LLNODE));
-  if (pnewnode == NULL) {
-    /* failed to allocate memory for new node */
-    return -1;
-  }
-
-  /* link the data to the node the link the node into the list */
-  pnewnode->data = pdata;
-
-  /* special processing if we are adding to empty list
-   * verify parameters are sensible for this situation
-   */
-  if ((pll->head == NULL) || (pll->tail == NULL)) {
-    /* head, tail and pnode must all be null or something's wrong */
-    if (! ((pll->head == NULL) && (pll->tail == NULL) && (pnode == NULL))) {
-      free (pnewnode);
-      return -2;
-    }
-
-    /* adding into an empty list - this is the one and only node */
-    pnewnode->prev = pnewnode->next = NULL;
-    pll->head = pll->tail = pnewnode;
-    return 0;
-  }
-
-  /* we are adding after the current node into a non empty list
-   * possibly the end of the list
-   */
-  pnewnode->prev = pnode;
-  pnewnode->next = pnode->next;
-  pnode->next = pnewnode;
-  if (pll->tail == pnode) {
-    /* adding to end of list */
-    pll->tail = pnewnode;
-  } else {
-    /* not at end of the list */
-    pnewnode->next->prev = pnewnode;
-  }
-
-  return 0;
+template<typename T>
+size_t LinkedList<T>::size() {
+    return num_elements;
 }
--- src/common/linkedlist.hpp
+++ src/common/linkedlist.hpp
@@ -1,39 +1,115 @@
 /** Header file for linked list
  *
- * @author Blake Nelson, Alex Miera
+ * @author Blake Nelson, Alex Miera, Patrick Critz
  *
  */
 
-#ifndef LINKEDLIST_H
-#define LINKEDLIST_H
+#pragma once
 
+/** Linked List
+ * This class represents a doubly linked list structure
+ * where the nodes all contain data of type T.
+ *
+ * Examples:
+ * LinkedList<int>* IntList = new LinkedList<int>(); (All of the nodes contain integers)
+ * IntList->push_back(0); //Both head and tail contain pointers to the same node with data 0
+ * LinkedList<Int>::Node* MyNode = IntList->push_back(10); //IntList: (0, 10) and a pointer to
+ *                                                           the node that was just created is stored in MyNode
+ * IntList->insert_before(MyNode, 5); //IntList: (0, 5, 10)
+ * IntList->remove(MyNode); //IntList: (0, 5)
+ * IntList->pop_back(); //IntList: (0)
+ * delete IntList;
+ */
+template<typename T>
 class LinkedList {
   
-  public:
-    typedef struct llnode {
-      struct llnode *prev;       /**< pointer to previous node */
-      struct llnode *next;       /**< pointer to next node     */
-      void* data;          /**< pointer to the data  */
-    } LLNODE;
+    public:
 
-    typedef struct llist {
-      LLNODE *head;              /**< pointer to head node of ll */
-      LLNODE *tail;              /**< pointer to tail node of ll */
-    } LLIST;
+        struct Node {
+            Node* prev;       /**< pointer to previous node */
+            Node* next;       /**< pointer to next node     */
+            T data;          /**< pointer to the data  */
+        };
 
-    LLIST* create (void);
-    int destroy (LLIST* pll);
-    int addhead (LLIST* pll, void* pdata);
-    int addtail (LLIST* pll, void* pdata);
-    LLNODE* get_first_node (LLIST* pll);
-    LLNODE* get_last_node (LLIST* pll);
-    LLNODE* get_next_node (LLNODE* pnode);
-    LLNODE* get_prev_node (LLNODE* pnode);
-    int add_before (LLIST* pll, LLNODE* pnode, void* pdata);
-    int add_after (LLIST* pll, LLNODE* pnode, void* pdata);
-    int deletenode (LLIST* pll, LLNODE* pnode);
+        LinkedList(); //Default Constructor
+        ~LinkedList(); //Default Destructor
 
-  private:
+    private:
 
+        Node* head;
+        Node* tail;
+        size_t num_elements; //cached for quick access
+
+    public:
+
+        //////////////////////
+        // GETTERS
+        //////////////////////
+
+        /** Get the current size of the number of the linked list
+         * @return the number of elements in the list from num_elements
+         */
+        size_t size();
+
+        /**
+         * @return a pointer to the head node
+         */
+        Node* get_head();
+
+        /**
+         * @return a pointer to the tail node
+         */
+        Node* get_tail();
+
+        //////////////////////
+        // INSERT OPERATORS
+        //////////////////////
+
+        /** Adds a node to the head of the list
+         * @param data - a pointer to the data to be stored in the node
+         * @return a pointer to the node that was created
+         */
+        Node* push_front(T data);
+
+        /** Adds a node to the tail of the list
+         * @param data - a pointer to the data to be stored in the node
+         * @return a pointer to the node that was created
+         */
+        Node* push_back(T data);
+
+        /** Adds data before the specified node
+         * If run successfully the "before" node will come after the node containing T
+         * @param before - a pointer to the node to put this data before
+         * @param data - a pointer to the data to be stored in the node
+         * @return a pointer to the node that was created or nullptr if the before node is not in the list
+         */
+        Node* insert_before(Node* before, T data);
+
+        /** Adds data after the specified node
+         * If run successfully the "after" node will come before the node containing T
+         * @param after - a pointer to the node to put this data after
+         * @param data - a pointer to the data to be stored in the node
+         * @return a pointer to the node that was created or nullptr if the before node is not in the list
+         */
+        Node* insert_after(Node* after, T data);
+
+        //////////////////////
+        // REMOVE OPERATORS
+        //////////////////////
+
+        /** Removes the first node from the list
+         * @return the last value of head
+         */
+        T pop_front();
+
+        /** Removes the last node from the list
+         * @return the last value of tail
+         */
+        T pop_back();
+
+        /** Removes the specified node from the list
+         * @param node_to_remove
+         * @return 0 if successful or a negative number in the event of an error
+         */
+        int remove_node(Node* node_to_remove);
 };
-#endif
